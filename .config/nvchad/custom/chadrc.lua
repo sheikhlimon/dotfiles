f18b07a61f08c4bf3566e3e447323c06e49b685e
sheikhlimon <sheikhlimon404@gmail.com>
2022-08-31 16:39:56 +0600

    update: modified nvchad

--- .config/nvchad/custom/chadrc.lua
+++ .config/nvchad/custom/chadrc.lua
@@ -1,25 +1,7 @@
 local M = {}
 
-local override = require "custom.override"
-
-M.plugins = {
-   options = {
-      lspconfig = {
-         setup_lspconf = "custom.plugins.lspconfig"
-      },
-   },
-
-   override = {
-      ["kyazdani42/nvim-tree.lua"] = override.nvimtree,
-      ["nvim-treesitter/nvim-treesitter"] = override.treesitter,
-      ["lukas-reineke/indent-blankline.nvim"] = override.blankline,
-   },
-
-   user = require "custom.plugins",
-}
-
 M.ui = {
-   theme = "onedark",
+  theme = "onedark",
 }
 
 M.mappings = require "custom.mappings"
--- .config/nvchad/custom/mappings.lua
+++ .config/nvchad/custom/mappings.lua
@@ -3,13 +3,7 @@ local M = {}
 M.general = {
    i = {
       ["jj"] = {"<esc>", "remapped normal mode jj"}
-   }
-}
-
-M.treesitter = {
-   n = {
-      ["<leader>cu"] = { "<cmd> TSCaptureUnderCursor <CR>", "  find media" },
    },
-}
+} 
 
 return M
