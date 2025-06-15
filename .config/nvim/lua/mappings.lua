9681e2eeb56e440fc88d75c6799ff094add5585c
sheikhlimon <sheikhlimon404@gmail.com>
2025-06-16 01:00:02 +0600

    vscode nvim waybar

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -11,6 +11,10 @@ map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 map("n", "<leader>s", "<cmd>w<CR>", { desc = "Save file" })
 
+-- Up, Down
+map("n", "j", "gj", { desc = "Up", noremap = true })
+map("n", "k", "gk", { desc = "Down", noremap = true })
+
 map("n", "<leader>ih", function()
   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
 end)
