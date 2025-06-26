9a9ea35f03858def5cd7ffca916f8a9e5d710f68
sheikhlimon <sheikhlimon404@gmail.com>
2025-06-26 18:09:50 +0600

    modified: nvim

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,24 +1,25 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "html", "cssls" }
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+local servers = {
+  "vtsls",
+  "html",
+  "cssls",
+  "tailwindcss",
+  "svelte",
+  "lua_ls",
+  "graphql",
+  "emmet_ls",
+  "prismals",
+  "pyright",
+  "clangd",
+  "prettier",
+  "biome",
+  "stylua",
+  "isort",
+  "black",
+  "clang-format",
+  "pylint",
+  "eslint_d",
+}
+vim.lsp.enable(servers)
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,56 +3,56 @@ return {
 
   --------------------------------------- default plugins -----------------------------------------
 
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   dependencies = {
-  --     -- {
-  --     --   "supermaven-inc/supermaven-nvim", --disabled for now
-  --     --   opts = {},
-  --     -- },
-  --     {
-  --       -- snippet plugin
-  --       "L3MON4D3/LuaSnip",
-  --       config = function(_, opts)
-  --         require("luasnip").config.set_config(opts)
-  --
-  --         local luasnip = require "luasnip"
-  --
-  --         luasnip.filetype_extend("javascriptreact", { "html" })
-  --         luasnip.filetype_extend("typescriptreact", { "html" })
-  --         luasnip.filetype_extend("svelte", { "html" })
-  --
-  --         require "nvchad.configs.luasnip"
-  --       end,
-  --     },
-  --
-  --     {
-  --       "hrsh7th/cmp-cmdline",
-  --       event = "CmdlineEnter",
-  --       config = function()
-  --         local cmp = require "cmp"
-  --
-  --         cmp.setup.cmdline("/", {
-  --           mapping = cmp.mapping.preset.cmdline(),
-  --           sources = { { name = "buffer" } },
-  --         })
-  --
-  --         cmp.setup.cmdline(":", {
-  --           mapping = cmp.mapping.preset.cmdline(),
-  --           sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
-  --           matching = { disallow_symbol_nonprefix_matching = false },
-  --         })
-  --       end,
-  --     },
-  --   },
-  --
-  -- opts = function(_, opts)
-  --   opts.sources[1].trigger_characters = { ".", "-" }
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      -- {
+      --   "supermaven-inc/supermaven-nvim", --disabled for now
+      --   opts = {},
+      -- },
+      {
+        -- snippet plugin
+        "L3MON4D3/LuaSnip",
+        config = function(_, opts)
+          require("luasnip").config.set_config(opts)
 
-  -- Leave supermaven out (commented)
-  -- table.insert(opts.sources, 1, { name = "supermaven" })
-  --   end,
-  -- },
+          local luasnip = require "luasnip"
+
+          luasnip.filetype_extend("javascriptreact", { "html" })
+          luasnip.filetype_extend("typescriptreact", { "html" })
+          luasnip.filetype_extend("svelte", { "html" })
+
+          require "nvchad.configs.luasnip"
+        end,
+      },
+
+      {
+        "hrsh7th/cmp-cmdline",
+        event = "CmdlineEnter",
+        config = function()
+          local cmp = require "cmp"
+
+          cmp.setup.cmdline("/", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = { { name = "buffer" } },
+          })
+
+          cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+            matching = { disallow_symbol_nonprefix_matching = false },
+          })
+        end,
+      },
+    },
+
+    opts = function(_, opts)
+      opts.sources[1].trigger_characters = { ".", "-" }
+
+      -- Leave supermaven out
+      -- table.insert(opts.sources, 1, { name = "supermaven" })
+    end,
+  },
 
   {
     "neovim/nvim-lspconfig",
@@ -61,9 +61,45 @@ return {
     end,
   },
 
-  -- {
-  --   "stevearc/conform.nvim",
-  --   event = "BufWritePre",
-  --   opts = require "configs.conform",
-  -- },
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    opts = require "configs.conform",
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "prisma",
+        "markdown",
+        "markdown_inline",
+        "svelte",
+        "graphql",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "query",
+        "vimdoc",
+        "c",
+      },
+    },
+    dependencies = {
+      {
+        "windwp/nvim-ts-autotag",
+        config = function()
+          require("nvim-ts-autotag").setup()
+        end,
+      },
+    },
+  },
 }
