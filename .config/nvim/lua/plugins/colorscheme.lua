8226d4a0b4b2457597cf1c722466d4732ce0e072
sheikhlimon <sheikhlimon404@gmail.com>
2025-08-06 17:31:39 +0600

    add vague theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "vague2k/vague.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("vague").setup {
+        transparent = true,
+      }
+      -- vim.cmd "colorscheme vague"
+    end,
+  },
+  {
     "savq/melange-nvim",
     priority = 1000,
     config = function()
