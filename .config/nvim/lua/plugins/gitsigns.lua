f2c6a1ef3149de4d510bf446a337f6d92d32a5f5
sheikhlimon <sheikhlimon404@gmail.com>
2025-08-12 11:41:27 +0600

    added custom gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,24 +1,28 @@
--- Adds git related signs to the gutter, as well as utilities for managing changes
 return {
   "lewis6991/gitsigns.nvim",
   opts = {
-    -- See `:help gitsigns.txt`
-    --   signs = {
-    --     add = { text = "+" },
-    --     change = { text = "~" },
-    --     delete = { text = "_" },
-    --     topdelete = { text = "‾" },
-    --     changedelete = { text = "~" },
-    --   },
-    --   signs_staged = {
-    --     add = { text = "+" },
-    --     change = { text = "~" },
-    --     delete = { text = "_" },
-    --     topdelete = { text = "‾" },
-    --     changedelete = { text = "~" },
-    --   },
+    signs = {
+      add = { hl = "GitGutterAdd", text = "+" },
+      change = { hl = "GitGutterChange", text = "~" },
+      delete = { hl = "GitGutterDelete", text = "_" },
+      topdelete = { hl = "GitGutterDelete", text = "‾" },
+      changedelete = { hl = "GitGutterChange", text = "~" },
+    },
+    signs_staged = {
+      add = { hl = "GitGutterAdd", text = "+" },
+      change = { hl = "GitGutterChange", text = "~" },
+      delete = { hl = "GitGutterDelete", text = "_" },
+      topdelete = { hl = "GitGutterDelete", text = "‾" },
+      changedelete = { hl = "GitGutterChange", text = "~" },
+    },
+    current_line_blame = false,
     current_line_blame_opts = {
       delay = 0,
+      virt_text_pos = "eol",
     },
+    -- Optional: toggle signs, etc.
+    sign_priority = 6,
+    update_debounce = 100,
+    status_formatter = nil, -- Use default
   },
 }
