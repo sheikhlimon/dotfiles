d7bf9db8098362f180c764491680e384f7b7b483
sheikhlimon <sheikhlimon404@gmail.com>
2025-10-04 18:31:08 +0600

    commented out leetcode

--- nvim/.config/nvim/lua/plugins/leetcode.lua
+++ nvim/.config/nvim/lua/plugins/leetcode.lua
@@ -1,14 +1,14 @@
-return {
-  "kawre/leetcode.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-  },
-  opts = {
-    lang = "python3",
-    storage = {
-      home = "~/projects/leetcode",
-    },
-    cache = { update_interval = 60 * 60 * 24 },
-  },
-}
+-- return {
+--   "kawre/leetcode.nvim",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "MunifTanjim/nui.nvim",
+--   },
+--   opts = {
+--     lang = "python3",
+--     storage = {
+--       home = "~/projects/leetcode",
+--     },
+--     cache = { update_interval = 60 * 60 * 24 },
+--   },
+-- }
